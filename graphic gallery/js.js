ec6df4c9ef4f5e9419912910a0e0ff267bd5b97b
Rakesh Seal <rakeshseal48@gmail.com>
2023-07-08 20:10:29 +0530

    done

---
+++ graphic gallery/js.js
@@ -0,0 +1,12 @@
+// let pre = document.getElementById("img_pre");
+// let next= document.getElementById("img_next");
+
+// function NextImg(){
+//   pre.style.display="none";
+//   next.style.display="block";
+// }
+
+// function PreImg(){
+//   pre.style.display="block";
+//   next.style.display="none";
+// }
